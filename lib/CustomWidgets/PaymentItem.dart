50bccaba60013097bb29a065b6d572e706342f4f
Eduardo <eduardnol@gmail.com>
2023-11-22 13:36:45 +0100

    Corrected error in row creation

--- lib/CustomWidgets/PaymentItem.dart
+++ lib/CustomWidgets/PaymentItem.dart
@@ -15,48 +15,76 @@ class PaymentItem extends StatelessWidget {
         children: [
           Expanded(
             flex: 1,
-            child: Row(
-              children: [
-                TextButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: Text("Cancel"),
-                  style: ButtonStyle(
-                    foregroundColor:
-                        MaterialStateProperty.all<Color>(Colors.red),
-                  ),
-                ),
-                Spacer(),
-                TextButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: Text("Save"),
-                  style: ButtonStyle(
-                    foregroundColor:
-                        MaterialStateProperty.all<Color>(Colors.green),
-                  ),
-                ),
-              ],
-            ),
+            child: SaveCancelButtons(),
           ),
           Expanded(
             flex: 9,
             child: Column(
               children: [
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Expanded(child: Text("Title"), flex: 8),
-                    Expanded(child: Text("Sample Text"), flex: 2),
-                  ],
-                ),
+                PaymentRowItem(title: "Title", value: "Spotify"),
+                PaymentRowItem(title: "Price", value: "R\$ 16,90"),
+                PaymentRowItem(title: "Desctiption", value: "Music"),
+                PaymentRowItem(title: "Date", value: "01/01/2021"),
               ],
             ),
           ),
         ],
       ),
+    );
+  }
+}
+
+class PaymentRowItem extends StatelessWidget {
+  final String title;
+  final String value;
+
+  PaymentRowItem({
+    super.key,
+    required this.title,
+    required this.value,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Expanded(child: Text(title), flex: 8),
+        Expanded(child: Text(value), flex: 2),
+      ],
+    );
+  }
+}
+
+class SaveCancelButtons extends StatelessWidget {
+  const SaveCancelButtons({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        TextButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text("Cancel"),
+          style: ButtonStyle(
+            foregroundColor: MaterialStateProperty.all<Color>(Colors.red),
+          ),
+        ),
+        Spacer(),
+        TextButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text("Save"),
+          style: ButtonStyle(
+            foregroundColor: MaterialStateProperty.all<Color>(Colors.green),
+          ),
+        ),
+      ],
     );
   }
 }
