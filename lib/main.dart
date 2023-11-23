61caf0c2f1382986a68f40bb5e661926e3335c11
Eduardo <eduardnol@gmail.com>
2023-11-23 13:26:37 +0100

    Added sliver app bar

--- lib/main.dart
+++ lib/main.dart
@@ -43,12 +43,28 @@ class MyHomePage extends StatelessWidget {
     return CustomScrollView(
       slivers: [
         SliverAppBar(
-          title: Text(title),
           floating: true,
+          pinned: false,
           backgroundColor: Colors.green,
           expandedHeight: 200.0,
           flexibleSpace: FlexibleSpaceBar(
-            background: Image.network('https://picsum.photos/250?image=9'),
+            centerTitle: false,
+            title: Text("Hello There!",
+                style: TextStyle(
+                  color: Colors.green,
+                  fontWeight: FontWeight.bold,
+                )),
+            stretchModes: <StretchMode>[
+              StretchMode.zoomBackground,
+              StretchMode.blurBackground,
+              StretchMode.fadeTitle
+            ],
+            background: FittedBox(
+              fit: BoxFit.fitWidth,
+              child: Image.network(
+                  "https://images.unsplash.com/photo-1619970291267-0e61f239c59e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"),
+            ),
+            collapseMode: CollapseMode.pin,
           ),
           stretch: true,
         ),
