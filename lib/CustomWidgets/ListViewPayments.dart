b065e614660f0ac4f96fcd5bcd1d4f04a804ac05
Eduardo <eduardnol@gmail.com>
2024-04-21 13:28:43 +0200

    Added provider implementation to manage state more easily

--- lib/CustomWidgets/ListViewPayments.dart
+++ lib/CustomWidgets/ListViewPayments.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:payments/services/AuthService.dart';
+import 'package:provider/provider.dart';
 
 import '../Model/PaymentObject.dart';
 import '../services/ProviderWidget.dart';
